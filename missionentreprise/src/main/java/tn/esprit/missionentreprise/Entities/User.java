36bc59ae488ed139459ea750d56b4bf80795cf1c
ManelSaidani <msaidani486@gmail.com>
2023-05-13 16:00:12 +0100

    initialisation de projet

---
+++ missionentreprise/src/main/java/tn/esprit/missionentreprise/Entities/User.java
@@ -0,0 +1,4 @@
+package tn.esprit.missionentreprise.Entities;
+
+public class User {
+}
---
+++ missionentreprise/src/test/java/tn/esprit/missionentreprise/MissionentrepriseApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.missionentreprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionentrepriseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
